512ffc2114d8eb56abcb224d4cab4202c672408b
vsidera <siderravictor@gmail.com>
2020-10-10 14:33:37 +0300

    add db relationships

--- gallery/models.py
+++ gallery/models.py
@@ -5,6 +5,8 @@ class Images(models.Model):
     image_link = models.ImageField(upload_to='images/')
     title = models.CharField(max_length=80)
     description = models.TextField()
+    category = models.ForeignKey('Categories', on_delete=models.CASCADE, default=1)
+    location = models.ForeignKey('Locations', on_delete=models.CASCADE, default=1)
    
     def __str__(self):
         return self.title
