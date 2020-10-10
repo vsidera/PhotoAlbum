b70232df1b5ab9c25959dd60f5931c82dc699b21
vsidera <siderravictor@gmail.com>
2020-10-10 14:32:15 +0300

    add locations and categories models

--- gallery/models.py
+++ gallery/models.py
@@ -6,3 +6,18 @@ class Images(models.Model):
     title = models.CharField(max_length=80)
     description = models.TextField()
    
+    def __str__(self):
+        return self.title
+
+class Categories(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name     
+
+class Locations(models.Model):
+    town = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)           
+
+    def __str__(self):
+        return self.city
