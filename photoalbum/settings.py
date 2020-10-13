5a23e8beee78f75ec003ae54f2ddbe38c6b54d96
vsidera <siderravictor@gmail.com>
2020-10-13 12:54:00 +0300

    edit Engine

--- photoalbum/settings.py
+++ photoalbum/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'photoalbum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
